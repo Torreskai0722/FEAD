010b4f37e9e9e2a5324c3e03a98b986fe23dd7e5
Liangkai Liu <36166171+Torreskai0722@users.noreply.github.com>
2023-03-30 10:31:46 -0400

    Update csv_load.sql

--- EMS-dataset-analysis/codes/csv_load.sql
+++ EMS-dataset-analysis/codes/csv_load.sql
@@ -28,3 +28,172 @@ INTO TABLE emsdata
 FIELDS TERMINATED BY '	' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- CREATE TABLE emsdata (
+--     rowkey VARCHAR(50) NOT NULL,
+--     ems_value VARCHAR(300) NOT NULL,
+--     ems_time VARCHAR(50) NOT NULL,
+--     imei VARCHAR(50) NOT NULL,
+--     gpsno VARCHAR(50) NOT NULL,
+--     truckid VARCHAR(100) NOT NULL,
+--     itemid VARCHAR(50) NOT NULL,
+--     model VARCHAR(20) NOT NULL,
+--     truckno VARCHAR(20) NOT NULL,
+--     orgcode VARCHAR(20) NOT NULL,
+--     lat VARCHAR(50),
+--     lng VARCHAR(50),
+--     course VARCHAR(50),
+--     triggertime VARCHAR(50),
+--     province VARCHAR(50),
+--     city VARCHAR(50),
+--     county VARCHAR(50),
+--     address VARCHAR(200),
+--     r_name VARCHAR(50),
+--     r_level VARCHAR(50),
+--     tdate VARCHAR(50) NOT NULL,
+--     PRIMARY KEY (rowkey)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- use fuel_efficiency_new;
+-- CREATE TABLE emsdata_3 (
+--     rowkey VARCHAR(50) NOT NULL,
+--     ems_value VARCHAR(300) NOT NULL,
+--     brake_pos VARCHAR(50),
+--     torque VARCHAR(50),
+--     tap_pos VARCHAR(50),
+--     ems_time VARCHAR(50) NOT NULL,
+--     imei VARCHAR(50) NOT NULL,
+--     gpsno VARCHAR(50) NOT NULL,
+--     truckid VARCHAR(100) NOT NULL,
+--     itemid VARCHAR(50) NOT NULL,
+--     model VARCHAR(20) NOT NULL,
+--     truckno VARCHAR(20) NOT NULL,
+--     orgcode VARCHAR(20) NOT NULL,
+--     lat VARCHAR(50),
+--     lng VARCHAR(50),
+--     course VARCHAR(50),
+--     triggertime VARCHAR(50),
+--     province VARCHAR(50),
+--     city VARCHAR(50),
+--     county VARCHAR(50),
+--     address VARCHAR(200),
+--     r_name VARCHAR(50),
+--     r_level VARCHAR(50),
+--     tdate VARCHAR(50) NOT NULL,
+--     PRIMARY KEY (rowkey)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- copy data to /var/lib/mysql/fuel_efficiency_new
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200406.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200407.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200408.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200409.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200410.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+use fuel_efficiency_new;
+-- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+LOAD DATA INFILE 'ems_yingche_20200411.txt'
+INTO TABLE emsdata_3 
+FIELDS TERMINATED BY '	' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200412.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200413.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200414.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200415.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200416.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200417.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200418.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
+
+-- use fuel_efficiency_new;
+-- -- LOAD DATA INFILE 'dwa_inceptio_ems_detail_location_manual.csv'
+-- LOAD DATA INFILE 'ems_yingche_20200419.txt'
+-- INTO TABLE emsdata_3 
+-- FIELDS TERMINATED BY '	' 
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
